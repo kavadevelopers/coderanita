a421c877bed41b9b63b6c1cd561751a5c9c53e17
Kava Developers <mehul9921@gmail.com>
2020-12-18 20:51:52 +0530

    Create Api.php

---
+++ application/controllers/Api.php
@@ -0,0 +1,29 @@
+<?php
+class Api extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function get_setting()
+	{
+		$setting = get_setting();
+		retJson(['bitwallet' => $setting['bitwallet'],'comission' => $setting['comission']]);
+	}
+
+	public function deposit()
+	{
+		$data = [
+			'bank'				=> $this->input->post('bank'),
+			'deposit_id'		=> $this->input->post('deposit_id'),
+			'amount'			=> $this->input->post('amount'),
+			'deptype'			=> $this->input->post('deptype'),
+			'file'				=> $this->input->post('file'),
+			'url'				=> $this->input->post('url'),
+			'created_at'		=> date('Y-m-d H:i:s')
+		];
+		$this->db->insert('deposit',$data);
+	}
+}
